7c05bfc3f461c4be7f2252793511272b651e8df0
Akah <robin.white280@gmail.com>
2020-03-07 19:12:21 +0100

    further simplified font functions

--- src/graphics/font.lisp
+++ src/graphics/font.lisp
@@ -7,12 +7,8 @@
 ;; foreach char get location on image
 ;; return list of char-locations with source as render-items to be renderered in renderer
 
-(defun split-string (string)
-  (loop for x from 1 to (length string)
-     collect (character (subseq string (1- x) x))))
-
-(defun get-coordinates (chars)
-  (mapcar (lambda (char) (char-code char)) chars))
-
 (defun string-to-coordinates (string)
-  (get-coordinates (split-string string)))
+  "convert string to list of ascii characters"
+  (mapcar (lambda (char) (char-code char)
+	     (loop for x from 1 to (length string)
+		collect (character (subseq string (1- x) x))))))
