e4f54fc2e0f95faaae4d944f716114d327f6c233
Akah <robin.white280@gmail.com>
2020-05-31 11:55:01 +0200

    update files (not working)

--- src/graphics/font.lisp
+++ src/graphics/font.lisp
@@ -2,16 +2,15 @@
 
 (in-package #:pkmn)
 
-;; f(string) => [render-item]
-;; take string input
-;; foreach char get location on image
-;; return list of char-locations with source as render-items to be renderered in renderer
+;; f(string) => [1D coordinates matching font image]
+;; * take string input
+;; * foreach char get location on image
+;; * return list of char-locations with source as render-items to be renderered
+;;   in renderer.
+;; 33 means space is -1, not shown in text image but when -1; need to remember ;; to leave a space of x pixels
 
-
-;; 33 means space is -1, not shown in text image
-;; but when -1; need to remember to leave a space of x pixels
 (defun string-to-coordinates (string)
-  "convert string to list of ascii characters shifted by 33"
-  (mapcar (lambda (char) (- (char-code char) 33))
+  "convert string to list of ascii characters shifted by 32"
+  (mapcar (lambda (char) (- (char-code char) 32))
 	    (loop for x from 1 to (length string)
 	       collect (character (subseq string (1- x) x)))))
--- src/graphics/game.lisp
+++ src/graphics/game.lisp
@@ -1,31 +1,32 @@
 ;;;; game.lisp
 
+;;; TODO:
+;; * Seperate functions into files i.e input handling, main game loop, drawing
+;;   functions, loading files.
+;; * Correct game loop
+
 (in-package #:pkmn)
 
-(defparameter *scale* 3)
+(defparameter *scale* 2)
 (defparameter *width*  (* 13 16 *scale*))
 (defparameter *height* (* 11 16 *scale*))
+(defparameter *tile-size* (* *scale* 16))
 
 (defparameter *x* 0)
 (defparameter *y* 0)
 
 (defparameter block-time 0)
 
+(defparameter font-image nil)
+
 (defun render-clear (renderer)
   (sdl2:set-render-draw-color renderer 255 255 255 0)
   (sdl2:render-clear renderer))
 
-(defun test-render-fill-rects (renderer)
-  (multiple-value-bind (rects num)
-      (apply #'sdl2:rects*
-             (loop :for x :upto 5
-                :collect (sdl2:make-rect (+ 500 (* x 10)) 400 8 8)))
-    (sdl2:set-render-draw-color renderer 255 0 255 255)
-    (sdl2:render-fill-rects renderer rects num)))
-
 (defun draw (renderer)
   (sdl2:set-render-draw-color renderer 255 0 0 0)
-  (sdl2:render-fill-rect renderer (sdl2:make-rect *x* *y* 16 16)))
+  (sdl2:render-fill-rect
+   renderer (sdl2:make-rect *x* *y* *tile-size* *tile-size*)))
 
 (defun draw-player-stat-holder (renderer)
   (sdl2:set-render-draw-color renderer 0 0 0 0)
@@ -51,20 +52,37 @@
 (defun print-debug (level string)
   (format t "[~a] ~a~%" level string))
 
-(defun draw-img (src x y renderer)
+(defun load-and-draw-img (src x y renderer)
   (let* ((image (sdl2-image:load-image src))
 	 (width (sdl2:surface-width image))
 	 (height (sdl2:surface-height image))
 	 (dst-rect (sdl2:make-rect x y (scale width) (scale height)))
 	 (texture (sdl2:create-texture-from-surface renderer image)))
-    (sdl2:render-copy renderer texture :dest-rect dst-rect)))
+    (sdl2:render-copy renderer texture :dest-rect dst-rect)
+    (sdl2:destroy-texture texture)
+    (sdl2:free-rect dst-rect)))
 
-(defun main-loop (renderer)
+(defun load-media ()
+  (let* ((stem "/home/rob/quicklisp/local-projects/cl-pkmn/res/img/")
+	 (font (concatenate 'string stem "font-v2.png")))
+    (setf font-image (sdl2-image:load-image font))
+    (print-debug :info (format nil "Loaded file: ~a" font))))
+
+(defun draw-img (img x y renderer window)
+  (let* ((width (sdl2:surface-width img))
+	 (height (sdl2:surface-height img))
+	 (dst-rect (sdl2:make-rect x y (scale width) (scale height))))
+    (sdl2:blit-surface img
+		       (sdl2:make-rect 16 0 8 8)
+		       (sdl2:get-window-surface window)
+		       (sdl2:make-rect 0 0 8 8))
+    (sdl2:free-rect dst-rect)))
+
+(defun main-loop (renderer window)
   "main game loop called in init environment"
-  (render-clear renderer)
+  ;(render-clear renderer)
   ;;
-  (draw renderer)
-  ;;(draw-stack render-stack renderer)
+  (draw-img font-image 32 0 renderer window)
   ;;
   (sdl2:render-present renderer)
   (when (< 0 block-time)
@@ -80,26 +98,22 @@
 
 (defun handle-key (keysym)
   "take an input and map to output for the key"
-  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-    (sdl2:push-event :quit))
+  (case (sdl2:scancode keysym)
+    (:scancode-escape (sdl2:push-event :quit))
+    (:scancode-grave  (toggle-pause)))
   (unless (> block-time 1)
-    (setf block-time 60)
-    (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-grave)
-      (toggle-pause))
-    (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-left)
-      (setq *x* (- *x* 10)))
-    (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-right)
-      (setq *x* (+ 10 *x*)))
-    (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-up)
-      (setq *y* (- *y* 10)))
-    (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-down)
-      (setq *y* (+ 10 *y*)))))
+    (setf block-time 30) ;; ms?
+    (case (sdl2:scancode keysym)
+      (:scancode-left  (setq *x* (- *x* *tile-size*)))
+      (:scancode-right (setq *x* (+ *x* *tile-size*)))
+      (:scancode-up    (setq *y* (- *y* *tile-size*)))
+      (:scancode-down  (setq *y* (+ *y* *tile-size*))))))
 
 (defun init ()
   "initilise gui and start main loop"
   (write-line "init game")
   (sdl2:with-init (:everything)
-    (sdl2:with-window (win
+    (sdl2:with-window (window
 		       :title "PKMN"
 		       :w *width*
 		       :h *height*
@@ -107,9 +121,11 @@
 		       :y 37;(- 1080 *height*)
 		       :flags '(:shown))
       (sdl2:with-renderer (renderer
-			   win
+			   window
 			   :flags '(:accelerated))
 	(with-image-init
+	  ;; after inits, code to be run before loop starts
+	  (load-media)
 	  (sdl2:with-event-loop (:method :poll)
           (:keydown
            (:keysym keysym)
@@ -117,7 +133,7 @@
           (:idle
 	   ()
 	   (unless (value-pause)
-	     (main-loop renderer)))
+	     (main-loop window renderer)))
           (:quit
 	   ()
 	   (sdl2-image:quit)
